59fdef8b22328a1f8d0040f5da1b3330107e8607
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-05 09:35:32 +0700

    user controller

---
+++ controllers/users/userCtrl.js
@@ -0,0 +1,79 @@
+// Register
+const userRegisterCtrl = async (req, res) => {
+  try {
+    res.json({
+      status: 'success',
+      data: 'user register',
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+// Login
+const userLoginCtrl = async (req, res) => {
+  try {
+    res.json({
+      status: 'success',
+      data: 'user login',
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+// Profile
+const userProfileCtrl = async (req, res) => {
+  try {
+    res.json({
+      status: 'success',
+      data: 'Profile route',
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+// All
+const usersCtrl = async (req, res) => {
+  try {
+    res.json({
+      status: 'success',
+      data: 'all Users route',
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+// Delete
+const deleteUserCtrl = async (req, res) => {
+  try {
+    res.json({
+      status: 'success',
+      data: 'delete user route',
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+const updateUserCtrl = async (req, res) => {
+  try {
+    res.json({
+      status: 'success',
+      data: 'update user route',
+    });
+  } catch (error) {
+    res.json(error.message);
+  }
+};
+
+module.exports = {
+  userRegisterCtrl,
+  userLoginCtrl,
+  userProfileCtrl,
+  usersCtrl,
+  deleteUserCtrl,
+  updateUserCtrl,
+};
--- routes/users/userRoutes.js
+++ routes/users/userRoutes.js
@@ -1,77 +1,31 @@
 const express = require('express');
+const {
+  userRegisterCtrl,
+  userLoginCtrl,
+  usersCtrl,
+  userProfileCtrl,
+  deleteUserCtrl,
+  updateUserCtrl,
+} = require('../../controllers/users/userCtrl');
 
 const userRouter = express.Router();
 
 // POST/api/v1/users/register
-userRouter.post('/register', async (req, res) => {
-  try {
-    res.json({
-      status: 'success',
-      data: 'user register',
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+userRouter.post('/register', userRegisterCtrl);
 
 // POST/api/v1/users/login
-userRouter.post('/login', async (req, res) => {
-  try {
-    res.json({
-      status: 'success',
-      data: 'user login',
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+userRouter.post('/login', userLoginCtrl);
 
 // GET/api/v1/users/profile/:id
-userRouter.get('/profile/:id', async (req, res) => {
-  try {
-    res.json({
-      status: 'success',
-      data: 'Profile route',
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+userRouter.get('/profile/:id', userProfileCtrl);
 
 // GET/api/v1/users
-userRouter.get('/', async (req, res) => {
-  try {
-    res.json({
-      status: 'success',
-      data: 'all Users route',
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+userRouter.get('/', usersCtrl);
 
 // DELETE/api/v1/users/:id
-userRouter.delete('/:id', async (req, res) => {
-  try {
-    res.json({
-      status: 'success',
-      data: 'delete user route',
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+userRouter.delete('/:id', deleteUserCtrl);
 
 // PUT/api/v1/users/:id
-userRouter.put('/:id', async (req, res) => {
-  try {
-    res.json({
-      status: 'success',
-      data: 'update user route',
-    });
-  } catch (error) {
-    res.json(error.message);
-  }
-});
+userRouter.put('/:id', updateUserCtrl);
 
 module.exports = userRouter;
