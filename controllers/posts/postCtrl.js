4009aba8d43f8109e3a5b5121b422ead7d0eafdc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-08 21:00:58 +0700

    finish post creation

--- controllers/posts/postCtrl.js
+++ controllers/posts/postCtrl.js
@@ -1,18 +1,26 @@
 const Post = require('../../model/Post/Post');
 const User = require('../../model/User/User');
+const { appErr } = require('../../utils/appErr');
 
 // Create post
 const createPostCtrl = async (req, res, next) => {
-  const { title, description } = req.body;
+  const { title, description, category } = req.body;
   try {
     // 1.Find the user
+    console.log(req.userAuth);
     const author = await User.findById(req.userAuth);
+    console.log(author.isBlocked);
+    // check if the user is blocked
+    if (author.isBlocked) {
+      return next(appErr('Access denied, account is blocked', 403));
+    }
 
     // 2.Create the post
     const postCreated = await Post.create({
       title,
       description,
       user: author._id,
+      category,
     });
 
     // 3.Associate user to a post - Push the post into the user posts field
