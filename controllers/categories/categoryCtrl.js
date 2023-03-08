7f54246c669b3417d9f3a02e50b5f8a7a3723d15
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2023-03-08 19:56:42 +0700

    create category

--- controllers/categories/categoryCtrl.js
+++ controllers/categories/categoryCtrl.js
@@ -1,50 +1,60 @@
-const createCategoryCtrl = async (req, res) => {
+const Category = require('../../model/Category/Category');
+const { appErr } = require('../../utils/appErr');
+
+// create
+const createCategoryCtrl = async (req, res, next) => {
+  const { title } = req.body;
   try {
+    const category = await Category.create({ title, user: req.userAuth });
+
     res.json({
       status: 'success',
-      data: 'category created',
+      data: category,
     });
   } catch (error) {
-    res.json(error.message);
+    next(appErr(error.message));
   }
 };
 
-const getCategoryCtrl = async (req, res) => {
+// get
+const getCategoryCtrl = async (req, res, next) => {
   try {
     res.json({
       status: 'success',
       data: 'Category route',
     });
   } catch (error) {
-    res.json(error.message);
+    next(appErr(error.message));
   }
 };
 
-const deleteCategory = async (req, res) => {
+// delete
+const deleteCategoryCtrl = async (req, res, next) => {
   try {
     res.json({
       status: 'success',
       data: 'delete category route',
     });
   } catch (error) {
-    res.json(error.message);
+    next(appErr(error.message));
   }
 };
 
-const updateCategoryCtrl = async (req, res) => {
+// update
+const updateCategoryCtrl = async (req, res, next) => {
   try {
     res.json({
       status: 'success',
       data: 'update categories route',
     });
   } catch (error) {
-    res.json(error.message);
+    next(appErr(error.message));
   }
 };
 
 module.exports = {
   createCategoryCtrl,
   getCategoryCtrl,
-  deleteCategory,
+  deleteCategoryCtrl,
   updateCategoryCtrl,
 };
--- routes/categories/categoryRoutes.js
+++ routes/categories/categoryRoutes.js
@@ -2,20 +2,21 @@ const express = require('express');
 const {
   createCategoryCtrl,
   getCategoryCtrl,
-  deleteCategory,
+  deleteCategoryCtrl,
   updateCategoryCtrl,
 } = require('../../controllers/categories/categoryCtrl');
+const isLogin = require('../../middlewares/isLogin');
 
 const categoryRouter = express.Router();
 
 // POST/api/v1/categories
-categoryRouter.post('/', createCategoryCtrl);
+categoryRouter.post('/', isLogin, createCategoryCtrl);
 
 // GET/api/v1/categories/:id
 categoryRouter.get('/:id', getCategoryCtrl);
 
 // DELETE/api/v1/categories/:id
-categoryRouter.delete('/:id', deleteCategory);
+categoryRouter.delete('/:id', deleteCategoryCtrl);
 
 // PUT/api/v1/categories/:id
 categoryRouter.put('/:id', updateCategoryCtrl);
